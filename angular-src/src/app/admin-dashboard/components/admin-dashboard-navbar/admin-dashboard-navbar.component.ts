6f9010b2793837a4ef44b8d9ffc1054d1fcbefe3
Stephen Wright <sjw948@gmail.com>
2017-06-07 21:46:57 +0100

    toggle-able menu built with auto-hide / show based on screen size change

--- angular-src/src/app/admin-dashboard/components/admin-dashboard-navbar/admin-dashboard-navbar.component.ts
+++ angular-src/src/app/admin-dashboard/components/admin-dashboard-navbar/admin-dashboard-navbar.component.ts
@@ -12,4 +12,23 @@ export class AdminDashboardNavbarComponent implements OnInit {
   ngOnInit() {
   }
 
+  menuVisible: boolean = true;
+
+  setMenuStyle() {
+    if(screen.width > 1024) {
+      this.menuVisible = true;
+    } else {
+      this.menuVisible = false;
+    }
+  }
+
+  menuStyle() {
+    if(this.menuVisible == false) {
+      return {"max-height": "0", "min-height": "0"}
+    } else {
+      let minHeight = document.getElementsByClassName("navbar-list")[0].children.length * 53;
+      return {"max-height": "auto", "min-height": minHeight + "px"}
+    }
+  }
+
 }
