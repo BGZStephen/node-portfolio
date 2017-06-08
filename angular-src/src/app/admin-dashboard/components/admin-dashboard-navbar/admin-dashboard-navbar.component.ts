b1c976d41769109a8761d6ee61e2185bd2e1649e
Stephen Wright <sjw948@gmail.com>
2017-06-08 08:11:33 +0100

    added slide out sub menus for admin dashboard navigation bar

--- angular-src/src/app/admin-dashboard/components/admin-dashboard-navbar/admin-dashboard-navbar.component.ts
+++ angular-src/src/app/admin-dashboard/components/admin-dashboard-navbar/admin-dashboard-navbar.component.ts
@@ -13,6 +13,16 @@ export class AdminDashboardNavbarComponent implements OnInit {
   }
 
   menuVisible: boolean = true;
+  activeSubMenu: number = -1;
+
+  menuStyle() {
+    if(this.menuVisible == false) {
+      return {"max-height": "0", "min-height": "0"}
+    } else {
+      let minHeight = document.getElementsByClassName("navbar-list")[0].children.length * 53;
+      return {"max-height": "100vh", "min-height": minHeight + "px"}
+    }
+  }
 
   setMenuStyle() {
     if(screen.width > 1024) {
@@ -22,12 +32,20 @@ export class AdminDashboardNavbarComponent implements OnInit {
     }
   }
 
-  menuStyle() {
-    if(this.menuVisible == false) {
-      return {"max-height": "0", "min-height": "0"}
+  setActiveSubMenu(index) {
+    if(this.activeSubMenu == index) {
+      this.activeSubMenu = -1
     } else {
-      let minHeight = document.getElementsByClassName("navbar-list")[0].children.length * 53;
+      this.activeSubMenu = index
+    }
+  }
+
+  subMenuStyle(index) {
+    if(this.activeSubMenu == index) {
+      let minHeight = document.getElementsByClassName("navbar-secondary-list")[index].children.length * 51;
       return {"max-height": "auto", "min-height": minHeight + "px"}
+    } else {
+      return {"max-height": "0", "min-height": "0"}
     }
   }
 
