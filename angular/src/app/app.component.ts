2e9de1ea5a37f6e33246b50650d138c3d9c33235
Stephen Wright <sjw948@gmail.com>
2017-08-20 19:54:17 +0100

    rejig webpack config to fix styles as string bug, added global styles

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 import './styles/main.scss';
 
 @Component({
   selector: 'portfolio-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./styles/main.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'portfolio works!';
