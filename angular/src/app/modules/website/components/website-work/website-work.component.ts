67182e57e073b7321275275d5c939e14a0de286b
Stephen Wright <sjw948@gmail.com>
2017-09-02 09:58:50 +0100

    added work examples responsive view

--- angular/src/app/modules/website/components/website-work/website-work.component.ts
+++ angular/src/app/modules/website/components/website-work/website-work.component.ts
@@ -2,8 +2,8 @@ import { OnInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-website-work',
-  templateUrl: './website-work.component.html',
-  styleUrls: ['./website-work.component.scss']
+  templateUrl: './website-work.component.html'
+  // styleUrls: ['./website-work.component.scss']
 })
 export class WebsiteWorkComponent implements OnInit {
 
