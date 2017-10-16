4a0c43ff7d61ca1ab9f11dceccbe00d5f85416b7
Stephen Wright <sjw948@gmail.com>
2017-10-16 20:04:31 +0100

    config sample + gitignore update

---
+++ api/config-sample.js
@@ -0,0 +1,17 @@
+const configs = {
+  development: {
+    apiUrl: '$API_URL',
+    authorization: '$AUTHORIZATION_TOKEN',
+    database: '$DATABASE',
+  },
+  staging: {
+    apiUrl: '$API_URL',
+    authorization: '$AUTHORIZATION_TOKEN',
+    database: '$DATABASE',
+  },
+  production: {
+    apiUrl: '$API_URL',
+    authorization: '$AUTHORIZATION_TOKEN',
+    database: '$DATABASE',
+  }
+};
