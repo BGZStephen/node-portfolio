6b7de88bc1ac7c5dd67b658cf910b135b40d64ba
stephen <sjw948@gmail.com>
2018-06-27 07:33:48 +0100

    user creation endpoint, active flag for user

--- api/models/user.js
+++ api/models/user.js
@@ -1,14 +1,27 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = mongoose.Schema({
 	createdOn: Date,
 	email: {
 		type: String,
-		unique: true
+		unique: true,
 	},
-	firstName: String,
-	lastName: String,
+	name: String,
 	password: String,
+	active: { type: Boolean, default: false },
 });
+
+UserSchema.pre('save', function() {
+	if (this.isModified('password')) {
+		this.password = bcrypt.hashSync(this.password, 8);
+	}
+});
+
+UserSchema.methods = {
+	passwordsMatch(password) {
+		return bcrypt.compareSync(password, this.password);
+	},
+};
 
 module.exports = mongoose.model('User', UserSchema);
--- api/routes/public/users.js
+++ api/routes/public/users.js
@@ -1,10 +1,11 @@
 const config = require('../../config');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const mongoose = require('mongoose');
-const User = mongoose.model('User');
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 const rest = require('api/utils/rest');
+const validate = require('api/utils/validate').process;
+
+const User = mongoose.model('User');
 
 async function authenticate(req, res) {
 	const user = await User.findOne({ email: req.body.email });
@@ -15,7 +16,7 @@ async function authenticate(req, res) {
 		});
 	}
 
-	if (!await bcrypt.compare(req.body.password, user.password)) {
+	if (!user.passwordsMatch(req.body.password)) {
 		return res.status(403).send({
 			message: 'Authentication failed',
 		});
@@ -32,6 +33,26 @@ async function authenticate(req, res) {
 	});
 }
 
+async function create(req, res) {
+	const validationErrors = validate(req.body, {
+		name: { presence: true },
+		email: { presence: true },
+		password: { presence: true },
+	});
+
+	if (validationErrors) {
+		return res.error({ statusCode: 400, message: validationErrors });
+	}
+
+	const fields = ['name', 'email', 'password'];
+
+	const user = new User(_pick(req.body, fields));
+	await user.save();
+
+	res.json(user);
+}
+
 router.post('/authenticate', rest.asyncwrap(authenticate));
+router.post('/', rest.asyncwrap(create));
 
 module.exports = router;
