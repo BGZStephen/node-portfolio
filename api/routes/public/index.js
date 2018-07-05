06511a4ac04be6d7c1ce12afee3ee6f3e819653a
Stephen Wright <stephen@getawriggleon.com>
2018-07-05 08:13:54 +0100

    fix url

--- api/routes/public/index.js
+++ api/routes/public/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const authorizeRoute = require('../../services/auth.js').authorizeRoute;
 
 router.use('/users', require('./users'));
-router.use('work-examples', require('./work-examples'));
+router.use('/work-examples', require('./work-examples'));
 
 module.exports = router;
