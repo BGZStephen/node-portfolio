85428d6631f8c3cbdc04af5c5efe198f4222229d
stephen <sjw948@gmail.com>
2018-07-16 22:09:45 +0100

    move to res.error, fix amll bugs

--- api/routes/private/users.js
+++ api/routes/private/users.js
@@ -1,4 +1,3 @@
-const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const router = require('express').Router();
 const rest = require('api/utils/rest');
@@ -9,23 +8,19 @@ const ObjectId = mongoose.Types.ObjectId;
 async function load(req, res, next) {
 	const userId = req.params.id;
 	if (!userId) {
-		res.send(500).json({
-			message: 'No User ID provided',
-		});
+		return res.error({message: 'No User ID provided', statusCode: 400});
 	}
 
 	const user = await User.findById(userId);
 	if (!user) {
-		return res.send(400).json({
-			message: 'User not found',
-		});
+		return res.error({message: 'User not found', statusCode: 404});
 	}
 
 	req.context.user = user;
 	next();
 }
 
-async function getOne(req, res) {
+function getOne(req, res) {
 	const user = req.user;
 	res.json(user);
 }
@@ -37,30 +32,23 @@ async function index(req, res) {
 
 async function update(req, res) {
 	let user = req.user;
-	const updatableFields = ['email', 'firstName', 'lastName'];
+  const updatableFields = ['email', 'firstName', 'lastName'];
 
-	if (req.body.password) {
-		const passwordVerification = verifyPassword(
-			req.body.password,
-			user.password,
-		);
-		if (!passwordVerification) {
-			return res.send(500).json({
-				message: 'Current password does not match stored password',
-			});
+	if (req.body.newPassword && !req.body.currentPassword) {
+    return res.error({message: 'Please provide current password when trying to update your password', statusCode: 400});
+  }
+
+  if (req.body.newPassword && req.body.currentPassword) {
+		if (!user.passwordsMatch(req.body.currentPassword)) {
+			return res.error({message: 'Current password does not match stored password', statusCode: 400});
 		}
-		user.password = req.body.password;
+		user.password = req.body.newPassword;
 	}
 
-	const duplicateUserEmail = await User.findOne({ email: req.body.email });
+	const duplicateUser = await User.findOne({ email: req.body.email });
 
-	if (
-		duplicateUserEmail &&
-		!ObjectId(duplicateUserEmail._id).equals(user._id)
-	) {
-		return res.status(500).send({
-			message: 'Email address already in use',
-		});
+	if (duplicateUser && !ObjectId(duplicateUser._id).equals(user._id)) {
+		return res.error({message: 'Email address already in use', statusCode: 400});
 	}
 
 	user = _.assign(user, _.pick(req.body, updatableFields));
@@ -69,13 +57,9 @@ async function update(req, res) {
 	res.json(user);
 }
 
-function verifyPassword(passwordQuery, password) {
-	return bcrypt.compareSync(passwordQuery, password);
-}
-
 router.get('/', rest.asyncwrap(index));
 router.all('/:id*', rest.asyncwrap(load));
-router.get('/:id', rest.asyncwrap(getOne));
+router.get('/:id', getOne);
 router.put('/:id', rest.asyncwrap(update));
 
 module.exports = router;
